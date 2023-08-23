70ea8bb3567913cdb95ad3c938396a0a07903d1e
caioeax <caio.ea@aluno.ifsc.edu.br>
2023-08-23 20:19:17 +0000

    instalação do ESLint

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -1,29 +1,29 @@
 export default class cena0 extends Phaser.Scene {
-    constructor () {
-        super('cena0')
-    }
+  constructor () {
+    super('cena0')
+  }
 
-    preload () {
-        this.load.image('eduardo', './assets/pombo.jpg')
-    }
+  preload () {
+    this.load.image('eduardo', './assets/pombo.jpg')
+  }
 
-    create () {
-        this.add.image(400, 225, 'eduardo')
-        this.timer = 2
-        this.timedEvent = this.time.addEvent({
-          delay: 1000,
-          callback: this.countdown,
-          callbackScope: this,
-          loop: true
-        })
-      }
+  create () {
+    this.add.image(400, 225, 'eduardo')
+    this.timer = 2
+    this.timedEvent = this.time.addEvent({
+      delay: 1000,
+      callback: this.countdown,
+      callbackScope: this,
+      loop: true
+    })
+  }
 
-    update () {
-        this.timer -= 1
-            if (this.timer <= 0) {
-            this.imagem.destroy()
-            this.timedEvent.destroy()
-            this.game.scene.start('sala')
-        }
+  update () {
+    this.timer -= 1
+    if (this.timer <= 0) {
+      this.imagem.destroy()
+      this.timedEvent.destroy()
+      this.game.scene.start('sala')
     }
+  }
 }
--- cliente/js/index.js
+++ cliente/js/index.js
@@ -1,16 +1,16 @@
-import config from "./config.js"
+import config from './config.js'
 import cena0 from './cena0.js'
 
 class Game extends Phaser.Game {
-    constructor () {
-        super(config)
+  constructor () {
+    super(config)
 
-        this.scene.add('cena0', cena0)
+    this.scene.add('cena0', cena0)
 
-        this.scene.start('cena0')
-    }
+    this.scene.start('cena0')
+  }
 }
 
 window.onload = () => {
-    window.game = new Game()
+  window.game = new Game()
 }
