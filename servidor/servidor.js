aa7ea8bfef85777157677eeaf8c0cd72ba13de01
caioeax <caio.ea@aluno.ifsc.edu.br>
2023-08-23 19:38:38 +0000

    adicionado novos arquivos

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(express.static('./'))
+app.use(express.static('../cliente/'))
 app.listen(PORT, () =>
   console.log(`Servidor em execução na porta ${PORT}!`)
 )
