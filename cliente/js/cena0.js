bcfa3c0ab80e3cd5b5493b1db7c2ba5a6602ac9e
caioeax <caio.ea@aluno.ifsc.edu.br>
2023-08-28 19:42:47 +0000

    inserido código personagem e botao

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -4,23 +4,50 @@ export default class cena0 extends Phaser.Scene {
   }
 
   preload () {
-    this.load.spritesheet('abertura', '../assets/CapaAbertura-90D.png', {
+    this.load.spritesheet('abertura', '../assets/CapaDeAbertura.png', {
       frameWidth: 800,
       frameHeight: 450,
+    })
+
+    this.load.spritesheet('derek', '../assets/derek.png')({
+      frameWidth: 64,
+      frameHeight: 64,
+    })
+
+    this.load.spritesheet('direita', '../assets/direita.png')({
+      frameWidth: 64,
+      frameHeight: 64,
     })
   }
 
   create () {
-    this.abertura = this.add.sprite(0, 0, 'abertura')
+    this.abertura = this.add.sprite(250, 298, 'abertura')
+
+    /*Tentativa de Abertura*/
 
     this.anims.create({
       key: 'abertura',
       frames: this.anims.generateFrameNumbers('abertura', {
         start: 0,
-        end: 26
+        end: 35
       }),
       frameRate: 30,
       repeat: -1
     })
+
+    /*Botões*/
+
+    this.direita = thias.add.sprite(150, 400, 'direita', 0)
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.direita.setFrame(1)
+        this.personagem.anims.play('derek-direita', true)
+        this.personagem.setVelocityX(100)
+      })
+      .on('pointerup', () => {
+        this.direita.setFrame(0)
+        this.personagem.anims.play('derek-parado')
+        this.personagem.setVelocityX(0)
+      })
   }
 }
