678aa25732304575de7b769dc84afb3af53e84d6
caioeax <caio.ea@aluno.ifsc.edu.br>
2023-08-28 20:24:41 +0000

    retirado arquivos e inserido botoes e personagens

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -9,19 +9,36 @@ export default class cena0 extends Phaser.Scene {
       frameHeight: 450,
     })
 
-    this.load.spritesheet('derek', '../assets/derek.png')({
+    /* Personagem */
+    this.load.spritesheet('derek', '../assets/derek.png', {
       frameWidth: 64,
-      frameHeight: 64,
+      frameHeight: 64
     })
 
-    this.load.spritesheet('direita', '../assets/direita.png')({
+    /* Botões */
+    this.load.spritesheet('direita', '../assets/botoes/direita.png', {
       frameWidth: 64,
-      frameHeight: 64,
+      frameHeight: 64
+    })
+
+    this.load.spritesheet('cima', '../assets/botoes/cima.png', {
+      frameWidth: 64,
+      frameHeight: 64
+    })
+
+        this.load.spritesheet('esquerda', '../assets/botoes/esquerda.png', {
+      frameWidth: 64,
+      frameHeight: 64
+    })
+
+    this.load.spritesheet('baixo', '../assets/botoes/baixo.png', {
+      frameWidth: 64,
+      frameHeight: 64
     })
   }
 
   create () {
-    this.abertura = this.add.sprite(250, 298, 'abertura')
+    this.abertura = this.add.sprite(400, 225, 'abertura')
 
     /*Tentativa de Abertura*/
 
@@ -31,13 +48,19 @@ export default class cena0 extends Phaser.Scene {
         start: 0,
         end: 35
       }),
-      frameRate: 30,
+      frameRate: 4,
       repeat: -1
     })
 
+    this.abertura.anims.play('abertura')
+
+    /* Personagem */
+
+    this.personagem = this.physics.add.sprite(400, 225, 'derek')
+
     /*Botões*/
 
-    this.direita = thias.add.sprite(150, 400, 'direita', 0)
+    this.direita = this.add.sprite(150, 400, 'direita', 0)
       .setInteractive()
       .on('pointerdown', () => {
         this.direita.setFrame(1)
@@ -48,6 +71,45 @@ export default class cena0 extends Phaser.Scene {
         this.direita.setFrame(0)
         this.personagem.anims.play('derek-parado')
         this.personagem.setVelocityX(0)
+      })
+    
+    this.cima = this.add.sprite(700, 300, 'cima', 0)
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.cima.setFrame(1)
+        this.personagem.anims.play('derek-cima', true)
+        this.personagem.setVelocityY(-100)
+      })
+      .on('pointerup', () => {
+        this.cima.setFrame(0)
+        this.personagem.anims.play('derek-parado')
+        this.personagem.setVelocityY(0)
+      })
+    
+    this.esquerda = this.add.sprite(50, 400, 'esquerda', 0)
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.esquerda.setFrame(1)
+        this.personagem.anims.play('derek-esquerda', true)
+        this.personagem.setVelocityX(-100)
+      })
+      .on('pointerup', () => {
+        this.esquerda.setFrame(0)
+        this.personagem.anims.play('derek-parado')
+        this.personagem.setVelocityX(0)
+      })
+
+    this.baixo = this.add.sprite(700, 400, 'baixo', 0)
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.baixo.setFrame(1)
+        this.personagem.anims.play('derek-baixo', true)
+        this.personagem.setVelocityY(100)
+      })
+      .on('pointerup', () => {
+        this.baixo.setFrame(0)
+        this.personagem.anims.play('derek-parado')
+        this.personagem.setVelocityY(0)
       })
   }
 }
