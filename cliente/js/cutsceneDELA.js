7c17b270cfce47e847d66b9f82e14fd7d8bd8917
caioeax <caio.ea@aluno.ifsc.edu.br>
2023-09-28 21:26:46 +0000

    adicionado cutsceneDELA com texto e setas

---
+++ cliente/js/cutsceneDELA.js
@@ -0,0 +1,130 @@
+export default class cutsceneDELA extends Phaser.Scene {
+    constructor () {
+        super('cutsceneDELA');
+    }
+
+    preload () {
+        this.load.image('cutsceneDELA', 'assets/cutsceneDELA.png');
+
+        /*Full Screen*/
+
+        this.load.spritesheet('tela-cheia', './assets/FullScreenICO.png', {
+            frameWidth: 32,
+            frameHeight: 32
+        })
+    }
+
+    create () {
+        // Adicione as imagens e defina a visibilidade inicial
+        const cutsceneDELAImage = this.add.image(400, 225, 'cutsceneDELA').setAlpha(0);
+
+
+        const telaLargura = 800;
+        const telaAltura = 450;
+
+        const texto = "Dois detetives chegam em uma remota cidade do Amazonas para ver o que está acontecendo em um museu abandonado, Rodrigo e Patrícia, uma dupla de investigadores.";
+        const texto2 = "Dois detetives chegam em uma remota cidade do Amazonas para ver o que está acontecendo em um museu abandonado, Rodrigo e Patrícia, uma dupla de investigadores. Amazonas para ver o que está acontecendo em um museu abandonado, Rodrigo e Patrícia, uma dupla de investigadores.";
+        const texto3 = "Dois detetives chegam em uma remota cidade do Amazonas para ver o que está acontecendo em um museu abandonado, Rodrigo e Patrícia, uma dupla de investigadores."; // Substitua pelo seu texto real
+        const tamanhoFonte = Math.min(telaLargura * 0.02, telaAltura * 0.1); // Ajuste os valores 0.05 e 0.1 conforme necessário // NÃO ESTOU UTILIZANDO, PORÉM É SÓ BOTAR NO LUGAR DO *32* EM FONTSIZE, É UMA VARIÁVEL QUE FICA COMPATÍVEL COM O TAMANHO DA TELA.
+
+        this.mensagem = this.add.text(200, 15, texto, {
+            fontFamily: "Felipa",
+            fontSize: 28 + "px",
+            stroke: "#000000",
+            strokeThickness: 4,
+            wordWrap: {
+                width: telaLargura - 200, // Defina a largura máxima para evitar que o texto saia da tela, 
+                useAdvancedWrap: true,
+            }
+        })
+
+        this.mensagem = this.add.text(15, 150, texto2, {
+            fontFamily: "Felipa",
+            fontSize: 28 + "px",
+            stroke: "#000000",
+            strokeThickness: 4,
+            wordWrap: {
+                width: telaLargura - 10, // Defina a largura máxima para evitar que o texto saia da tela, 
+                useAdvancedWrap: true,
+            }
+        })
+
+        this.mensagem = this.add.text(15, 320, texto3, {
+            fontFamily: "Felipa",
+            fontSize: 28 + "px",
+            stroke: "#000000",
+            strokeThickness: 4,
+            wordWrap: {
+                width: telaLargura - 160, // Defina a largura máxima para evitar que o texto saia da tela, 
+                useAdvancedWrap: true,
+            }
+        })
+
+        // Crie botões para avançar e retroceder
+        const nextButton = this.add.text(750, 225, '->', {
+            fontSize: '32px',
+            fill: '#fff',
+            stroke: "#000000",
+            strokeThickness: 4,
+        });
+        nextButton.setOrigin(0.5);
+        nextButton.setInteractive();
+
+        const prevButton = this.add.text(50, 225, '<-', {
+            fontSize: '32px',
+            fill: '#fff',
+            stroke: "#000000",
+            strokeThickness: 4,
+        });
+        prevButton.setOrigin(0.5);
+        prevButton.setInteractive();
+
+        // Função para animar a transição para a próxima cena
+        const goToNextScene = () => {
+            this.scene.start('cutscene2');
+        };
+
+        // Função para animar a transição para a cena anterior
+        const goToPreviousScene = () => {
+            this.scene.start('cutscene'); // Substitua 'cena_anterior' pelo nome da cena anterior
+        };
+
+        // Configurar eventos de clique para os botões
+        nextButton.on('pointerdown', () => {
+            // Animação de Fade Out para a cena atual
+            fadeOut(cutsceneDELAImage, 1000, () => {
+                // Chame a função para avançar para a próxima cena
+                goToNextScene();
+            });
+        });
+
+        prevButton.on('pointerdown', () => {
+            // Animação de Fade Out para a cena atual
+            fadeOut(cutsceneDELAImage, 1000, () => {
+                // Chame a função para retroceder para a cena anterior
+                goToPreviousScene();
+            });
+        });
+
+        const fadeIn = (target, duration, onComplete) => {
+            this.tweens.add({
+                targets: target,
+                alpha: 1,
+                duration: duration,
+                onComplete: onComplete,
+            });
+        };
+
+        const fadeOut = (target, duration, onComplete) => {
+            this.tweens.add({
+                targets: target,
+                alpha: 0,
+                duration: duration,
+                onComplete: onComplete,
+            });
+        };
+
+        // Inicie a cena com o Fade In para 'cutsceneDELA'
+        fadeIn(cutsceneDELAImage, 1000);
+    }
+}
--- cliente/js/index.js
+++ cliente/js/index.js
@@ -8,6 +8,7 @@ import cutscene from './cutscene.js'
 import cutscene1 from './cutscene1.js'
 import cutscene2 from './cutscene2.js'
 import cutsceneDELE from './cutsceneDELE.js'
+import cutsceneDELA from './cutsceneDELA.js'
 import loading from './loading.js'
 
 class Game extends Phaser.Game {
@@ -23,9 +24,10 @@ class Game extends Phaser.Game {
     this.scene.add('cutscene1', cutscene1)
     this.scene.add('cutscene2', cutscene2)
     this.scene.add('cutsceneDELE', cutsceneDELE)
+    this.scene.add('cutsceneDELA', cutsceneDELA)
     this.scene.add('loading', loading)
 
-    this.scene.start('cutsceneDELE')
+    this.scene.start('abertura')
   }
 }
 
