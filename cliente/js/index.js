55b96964df8020692242993d8974163bc5ca31a8
caioeax <caio.ea@aluno.ifsc.edu.br>
2023-10-23 19:08:16 +0000

    colocado codigo audio no servidor e index.js

--- cliente/js/index.js
+++ cliente/js/index.js
@@ -16,6 +16,29 @@ class Game extends Phaser.Game {
   constructor () {
     super(config)
 
+    let iceServers
+    if (window.location.host === 'feira-de-jogos.sj.ifsc.edu.br') {
+      iceServers = [
+        {
+          urls: 'stun:feira-de-jogos.sj.ifsc.edu.br'
+        },
+        {
+          urls: 'turns:feira-de-jogos.sj.ifsc.edu.br',
+          username: 'adcipt',
+          credential: 'adcipt20232'
+        }
+      ]
+    } else {
+      iceServers = [
+        {
+          urls: 'stun:stun.l.google.com:19302'
+        }
+      ]
+    }
+    this.iceServers = { iceServers }
+    this.audio = document.querySelector('audio')
+
+
     this.socket = io() /* global io */
     this.socket.on('connect', () => {
       console.log('Connected to server!')
